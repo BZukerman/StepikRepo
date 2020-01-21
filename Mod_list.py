008034508e82cc4a5b41f2586a9644a7752b0139
BZukerman <BZukerman@mail.ru>
2020-01-21 12:20:42 +0500

    My 1st Python files in the repository

--- Mod_list.py
+++ Mod_list.py
@@ -1,28 +1,18 @@
-lst = [1, 2, 3, 4, 5, 6]
-Length = len(lst)
-print(Length)
-print(lst)
+# lst = [1, 2, 3, 4, 5, 6]
+lst = [10, 5, 8, 3]
+# print(Length)
+# print(lst)
 def modify_list(lst):
-    print(Length)
-    print(lst)
-    for x in lst:
-        h= lst.pop(x)
-        print(h)
-        rest = h % 2
-        print(rest)
+    Length = len(lst)
+    for x in range(Length - 1, -1, -1):
+        xi = lst[x]
+        rest = xi % 2
         if rest == 1:
-#            lst.remove(x)
-#            del(x)
-            lst[x] = h
-            print(lst)
+            lst.remove(xi)
         if rest == 0:
-            print("x:", x)
-            x = x//2
-            print("hx:",x)
-            lst[x] = x
-#        lst = z
+            lst[x] = xi//2
     return
 modify_list(lst)
 Length = len(lst)
-print(Length)
+# print(Length)
 print(lst)
