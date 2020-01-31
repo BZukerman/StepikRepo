b7038b5b8c22a0293a3769639757f5aab0eaf5b0
BZukerman <BZukerman@mail.ru>
2020-01-31 20:57:07 +0500

    My 1st Python files in the repository

--- Dict._Theory.py
+++ Dict._Theory.py
@@ -1,12 +1,12 @@
 d = {'C': 14, 'A': 12, 'T': 9, 'G': 18}
 for key in d:
-    print(key, end=' ')
+    print(key, end=' ')         # C A T G
 print()
 for key in d.keys():
-    print(key, end=' ')
+    print(key, end=' ')         # C A T G
 print()
 for value in d.values():
-    print(value, end=' ')
+    print(value, end=' ')       # 14 12 9 18
 print()
 for key, value in d.items():
-    print(key, value, end='; ')
+    print(key, value, end='; ') # C 14; A 12; T 9; G 18
