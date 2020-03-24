63beff20a874fd45df82f3120b7277e655f9a9b9
BZukerman <BZukerman@mail.ru>
2020-03-24 20:30:03 +0500

    My 1st Python files in the repository

--- Python_Programming/3. Functions, Dictionaries, Files, Modules/Average_Height.py
+++ Python_Programming/3. Functions, Dictionaries, Files, Modules/Average_Height.py
@@ -17,42 +17,26 @@ for Line in Inf:
     N_Row = N_Row + 1
 #    print(N, Line)
     Class_i = int(Line[0])
-#    N_i = N_i + 1
     Surname_i = Line[1]
     Height_i = int(Line[2])
-    List_i = [N_Row, Surname_i, Height_i]
-    print("1:", Class_i, Surname_i, Height_i)
-    D_h = Data.items()
-    Cl_h = Class
-    L_h = List
-    N_h = List[0]
-    Sn_h = List[1]
-    H_h = List[2]
-    if Cl_h == 0:
-        N_h = 1
-        Pair_i = {Cl_h: L_h}
-        Data.update(Pair_i)
-    else:
-        N_h = N_h + 1
-#    if N_h == 1:
-#        Pair_i = {Cl_h: L_h}
-#        Data.update(Pair_i)
-#        Data_i = Data(List_i)
-#        Exist = Data.get(Class_i)
-#    if Exist == Class_i and N_i > 1:
-#        N_i = N_i + 1
-#    if N_i > 1:
-#    if N_h > 1:
-#        List_i = Data.get(Class_i)
-        L_h.append([N_h, Surname_i, Height_i])
-        Pair_i = {Cl_h: L_h}
-        Data.update(Pair_i)
+    List_i = [0, Surname_i, Height_i]
+    print("1)", Class_i, ":", Surname_i, Height_i)
+    D_h = Data.get(Class_i)
+    N_h = D_h[0]
+#    L_h = D_h
+    Sn_h = D_h[1]
+    H_h = D_h[2]
+    if N_h == 0:
+        Pair_i = {Class_i: [1, Surname_i, Height_i]}
+        print("2)", Pair_i)
+    if N_h != 0:
+        D_h = D_h.append([0, Surname_i, Height_i])
+        Pair_i = {Class_i: [len(Line), Surname_i, Height_i]}
+        print("3)", Pair_i)
+    Data.update(Pair_i)
     print("Pair_i:", Pair_i)
-#    Data.update(Pair_i)
-#    Data_i.append(Pair_i)
-#    Data.append(Pair_i)
 Inf.close()
 # print(Class_i, " ", Data_i, end = "\n")
 print(Data)
 for Class, Data_i in Data.items():
-    print((Class, " "), Data_i, end = "\n")
+    print(Class, Data_i, end = "\n")
