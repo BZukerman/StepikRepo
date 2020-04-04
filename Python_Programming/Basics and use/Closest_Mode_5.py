e2077ee85bf91377acd1e977c19c4f313d3ec837
BZukerman <BZukerman@mail.ru>
2020-04-04 17:40:24 +0500

    My 1st Python files in the repository

--- Python_Programming/Basics and use/Closest_Mode_5.py
+++ Python_Programming/Basics and use/Closest_Mode_5.py
@@ -1,18 +1,21 @@
-def Closest_Mod_5(x):
-#    y = abs(x)
-    Rest = x % 5
-    if x >= 0 and Rest == 0:
+# Напишите реализацию функции closest_mod_5, принимающую в качестве единственного аргумента
+# целое число x и возвращающую самое маленькое целое число y, такое что:
+# y больше или равно x
+# y делится нацело на 5
+#
+def closest_mod_5(x):           # Объявление функции
+    Rest = x % 5                # Остаток от деления на 5
+    if x >= 0 and Rest == 0:    # Для кратных 5 полжительных чисел
         return x
-    if x >= 0 and Rest != 0:
+    if x >= 0 and Rest != 0:    # Для некратных 5 положительных чисел - ответ правее
         return (x + 5 - Rest)
-    if x < 0 and Rest == 0:
-        return (x + 5)
-    if x < 0 and Rest > 0:
+    if x < 0 and Rest == 0:     # Для кратных 5 отрицательных чисел
+        return x
+    if x < 0 and Rest > 0:      # Для некратных 5 отрицательных чисел - ответ правее
         return (x + 5 - Rest)
-    else:
+    else:                       # Не используется
         return("I don't know :(")
-#        print("Error")
 x = int(input())
-z = Closest_Mod_5(x)
+z = closest_mod_5(x)
 print(z)
 
