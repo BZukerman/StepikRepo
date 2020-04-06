425c79f0da77122ad240ae0f7dcbfc1bf1bc9b1c
BZukerman <BZukerman@mail.ru>
2020-04-06 20:21:12 +0500

    My 1st Python files in the repository

--- Python_Programming/Basics and use/Combinations.py
+++ Python_Programming/Basics and use/Combinations.py
@@ -1,2 +1,9 @@
 n, k = map(int, input().split())
 print("n:", n, "k:", k)
+def Comb(n, k)
+    if k == 0:
+        return 0
+    if k > n:
+        return = 0
+    Res = Comb(n - 1, k) + Comb(n - 1, k - 1)
+    return Res
