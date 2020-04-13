3610c4dbd42ac3eee8796216d52f3b943c815226
BZukerman <BZukerman@mail.ru>
2020-04-13 20:27:12 +0500

    My 1st Python files in the repository

---
+++ Python_Programming/Basics and use/Money_Box.py
@@ -0,0 +1,13 @@
+class MoneyBox:
+    def __init__(self, capacity):   # # конструктор с аргументом – вместимость копилки
+        def can_add(self, v):       # True, если можно добавить v монет
+            if capacity > val:
+                self.v = capacity - val
+        def add(self, v):           # положить v монет в копилку
+            capacity = val + v
+#        def print_val(self):
+#            print(self.v)
+capacity = 10
+val = 3
+# MoneyBox(capacity)
+print(MoneyBox(val))
