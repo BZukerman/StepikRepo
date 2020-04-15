84ceb737a89dee0ac3cad8721eb2b7709df12041
BZukerman <BZukerman@mail.ru>
2020-04-15 19:44:04 +0500

    My 1st Python files in the repository

--- Python_Programming/Basics and use/Money_Box.py
+++ Python_Programming/Basics and use/Money_Box.py
@@ -1,19 +1,48 @@
 class MoneyBox:
     def __init__(self, capacity):   # # конструктор с аргументом – вместимость копилки
-#        self.v = 0
-#        MoneyBox.Vol = 0
-        print(capacity, MoneyBox.Vol)
+        self.capacity = capacity
+        print("capacity:", capacity)
+        MoneyBox.W = False
 #
-        def can_add(self, v):       # True, если можно добавить v монет
-            self.capacity = capacity
-            self.v = v
-            if self.cap > Vol:
-                self.v = self.cap - Vol
+    def can_add(self, v):       # True, если можно добавить v монет
+#        self.capacity = MoneyBox.capacity
+        self.v = v
+        print("1:", "self.capacity:", self.capacity, "self.v:", self.v)
+        if self.capacity >= self.v:
+#            self.capacity = self.capacity - self.v
+            MoneyBox.W = True
+            print("1:", "W:", MoneyBox.W, "self.capacity:", self.capacity)
+            return MoneyBox.W
+        if self.capacity < self.v:
+            MoneyBox.W = False
+            print("1:", "W:", MoneyBox.W)
+            return MoneyBox.W
 #
-        def add(self, v):           # положить v монет в копилку
-            Vol = Vol + self.v
+    def add(self, v):           # положить v монет в копилку
+        self.v = v
+        print("2:", "self.capacity:", self.capacity, "self.v:", self.v, "MoneyBox.W:", MoneyBox.W)
+        self.can_add(self.v)
+        if MoneyBox.W == True:
+            self.capacity = self.capacity - self.v
+            print("self.capacity:", self.capacity)
+            return self.capacity
+        else:
+            print("Stop")
 #
-capacity = 10
-MoneyBox.Vol = 3
-v = MoneyBox(capacity)
-print(v)
+X = MoneyBox(10)
+# print(X)
+Coin = 3
+# v = MoneyBox(capacity)
+# print("X:", X)
+# Y = X.can_add(Coin)
+# print("Y:", Y)
+Z = X.add(Coin)     # 7
+print("Z:", Z)
+Z = X.add(Coin)     # 4
+print("Z:", Z)
+Z = X.add(Coin)
+print(Z)            # 1
+Z = X.add(Coin)
+print(Z)            # 1
+Z = X.add(Coin)
+print(Z)
