ed230f971129e90ca400e42554169cb1beb6a1b4
BZukerman <BZukerman@mail.ru>
2020-04-16 11:58:24 +0500

    My 1st Python files in the repository

--- Python_Programming/Basics and use/Money_Box.py
+++ Python_Programming/Basics and use/Money_Box.py
@@ -1,48 +1,59 @@
+# Реализуйте класс MoneyBox, для работы с виртуальной копилкой.
+# Каждая копилка имеет ограниченную вместимость, которая выражается целым числом
+# – количеством монет, которые можно положить в копилку. Класс должен поддерживать
+# информацию о количестве монет в копилке, предоставлять возможность добавлять монеты
+# в копилку и узнавать, можно ли добавить в копилку ещё какое-то количество монет,
+# не превышая ее вместимость.
+# Вид класса задан.
+# При создании копилки число монет в ней равно 0.
+#
 class MoneyBox:
-    def __init__(self, capacity):   # # конструктор с аргументом – вместимость копилки
-        self.capacity = capacity
-        print("capacity:", capacity)
-        MoneyBox.W = False
+    def __init__(self, capacity):   # Конструктор с аргументом вместимость копилки
+        self.capacity = capacity    # Емкость пустой копилки
+#        print("Volume:", capacity)
+        MoneyBox.W = False          # Признак, что можно пополнить, без начального значения нельзя!
 #
-    def can_add(self, v):       # True, если можно добавить v монет
-#        self.capacity = MoneyBox.capacity
-        self.v = v
-        print("1:", "self.capacity:", self.capacity, "self.v:", self.v)
-        if self.capacity >= self.v:
-#            self.capacity = self.capacity - self.v
+    def can_add(self, v):           # Возвращает True, если можно добавить v монет
+        self.v = v                  # Добавляемое число монет
+#        print("1:", "self.capacity:", self.capacity, "self.v:", self.v)
+        if self.capacity >= self.v:     # Если свободно больше или равно, чем добавка
             MoneyBox.W = True
-            print("1:", "W:", MoneyBox.W, "self.capacity:", self.capacity)
+#            print("1:", "W:", MoneyBox.W, "self.capacity:", self.capacity)
             return MoneyBox.W
-        if self.capacity < self.v:
+        if self.capacity < self.v:  # Если свободно меньше, чем добавка
             MoneyBox.W = False
-            print("1:", "W:", MoneyBox.W)
+#            print("1:", "W:", MoneyBox.W)
             return MoneyBox.W
 #
-    def add(self, v):           # положить v монет в копилку
-        self.v = v
-        print("2:", "self.capacity:", self.capacity, "self.v:", self.v, "MoneyBox.W:", MoneyBox.W)
-        self.can_add(self.v)
-        if MoneyBox.W == True:
-            self.capacity = self.capacity - self.v
-            print("self.capacity:", self.capacity)
-            return self.capacity
+    def add(self, v):               # Положить v монет в копилку
+        self.v = v                  # Добавка
+#        print("2:", "self.capacity:", self.capacity, "self.v:", self.v, "MoneyBox.W:", MoneyBox.W)
+        self.can_add(self.v)        # Влезет ли добавка?
+        if MoneyBox.W == True:      # Если можно добавить
+            self.capacity = self.capacity - self.v      # Уменьшаем свободное пространство
+#            print("You can add :", self.capacity)
+            return self.capacity        # Возвращаем  свободное пространство
         else:
-            print("Stop")
+#            print("Stop")
+            return False
 #
-X = MoneyBox(10)
-# print(X)
+Volume = 20
+X = MoneyBox(Volume)
 Coin = 3
-# v = MoneyBox(capacity)
-# print("X:", X)
-# Y = X.can_add(Coin)
-# print("Y:", Y)
-Z = X.add(Coin)     # 7
-print("Z:", Z)
-Z = X.add(Coin)     # 4
-print("Z:", Z)
-Z = X.add(Coin)
-print(Z)            # 1
-Z = X.add(Coin)
-print(Z)            # 1
-Z = X.add(Coin)
-print(Z)
+print("Volume: ", Volume, "Input:", Coin)
+# Z = X.add(Coin)     # 7
+# print("The rest is: ", Z)
+# Z = X.add(Coin)     # 4
+# print("The rest is: ", Z)
+# Z = X.add(Coin)     # 1
+# print("The rest is: ", Z)
+# Z = X.add(Coin)     # Stop
+# print("You can add only ", Z)            # 1
+# print(MoneyBox.W)
+# Тестовый цикл
+N = Volume // Coin
+print("N:", N)
+for i in range(N):
+    Z = X.add(Coin)
+#    print(i, " The rest is:", Z)
+print("You can add now only:", Z)
