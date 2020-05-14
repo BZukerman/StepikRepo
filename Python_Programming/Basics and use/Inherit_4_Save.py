6419eda50abd76da438e136e77d8d4d5b5293cf3
BZukerman <BZukerman@mail.ru>
2020-05-14 16:46:38 +0500

    14/05/2020

--- Python_Programming/Basics and use/Inherit_4_Save.py
+++ Python_Programming/Basics and use/Inherit_4_Save.py
@@ -1,12 +1,12 @@
 # Ввод данных при отладке
-Classes = 15
-Inher = [['GG','FF'],['AA'],['BB','AA'],['CC','AA'],['DD','BB CC'],['EE','DD'],['FF','DD'],['XX'],['YY','XX AA'],['ZZ','XX'],['VV','ZZ YY'],['WW','VV'],['QQ','PP'],['QQ','RR'],['QQ','SS']]
-#Classes = 10
-#Inher = [['AA'],['BB', 'AA'],['CC','AA'],['DD','BB CC'],['EE','DD'],['HH'],['KK'],['FF','HH KK'],['GG','CC FF'],['LL','GG']]
-Requests = 9
-Req = [['AA','GG'],['AA','ZZ'],['AA','WW'],['XX','WW'],['XX','QWE'],['AA','XX'],['XX','XX'],['ll','ll'],['QQ','QQ']]
+#Classes = 15
+#Inher = [['GG','FF'],['AA'],['BB','AA'],['CC','AA'],['DD','BB CC'],['EE','DD'],['FF','DD'],['XX'],['YY','XX AA'],['ZZ','XX'],['VV','ZZ YY'],['WW','VV'],['QQ','PP'],['QQ','RR'],['QQ','SS']]
+Classes = 10
+Inher = ['AA', 'BB : AA', 'CC : AA', 'DD : BB CC', 'EE : DD', 'HH', 'KK', 'FF : HH KK', 'GG : CC FF', 'LL : GG']
+#Requests = 9
+#Req = [['AA','GG'],['AA','ZZ'],['AA','WW'],['XX','WW'],['XX','QWE'],['AA','XX'],['XX','XX'],['ll','ll'],['QQ']]
 #Requests = 10
-#Req = [['AA','BB'],['BB','DD'],['CC','DD'],['DD','AA'],['CC','EE'],['GG','EE'],['KK','LL'],['AA','LL'],['KK','EE'],['BB','BB']]
+#Req = ['A B', 'B D', 'C D', 'D A', 'C E', 'G E', 'K L', 'A L', 'K E', 'B B']
 #
 #Inher_i = []
 #Inher = []
@@ -27,59 +27,96 @@ print("Inher:", Inher)
 #    Req_j = input().split()
 #    print("Req_j:", Req_j)
 #    Req.append(Req_j)
-print("Requests:", Requests)
-print("Req:", Req)
+#print("Requests:", Requests)
+#print("Req:", Req)
 # Словарь Relatives = {Keys : Pars}
 # Множество Keys.
 # Множество Pars. Метод add
 Pars = []
 Relatives = {}
 Keys = []
+# Values = []
 Vals = []
 for i in range(Classes):
     Val_i = []
     Val_ii = []
     Sum_i = []
     Mem_i = Inher[i]
+#    Len_i = len(Mem_i)
+#    Key_i = Mem_i[0]
+    Key_i = Mem_i
+#    Val_i = Mem_i[1]
     Len_i = len(Mem_i)
-    Key_i = Mem_i[0]
+    print("i:", i, "Key_i:", Key_i, "Mem_i:", Mem_i, "Len_i:", Len_i)
     Mem_Old = []
+#    Val_h = Relatives.get(Key_i)
+#    print("Val_h:", Val_h)
     if Key_i not in Keys:
         Keys.append(Key_i)
+#N_Keys = len(Keys)
+#print("N_Keys:", N_Keys)
+#    Keys.append(Key_i)
+#    NKeys = set(Keys)
+#for i in range(Classes):
     if Len_i == 1:
         Val_i = "object"
+#        Values.append(Val_i)
         Pair_i = {Key_i: Val_i}
         Pars.append(Val_i)
         Relatives.update(Pair_i)
-    if Len_i == 2:
+#        print("i:", i, "Key_i:", Key_i, "Val_i:", Val_i)
+    if Len_i > 1:
         if Key_i in Keys:
             Mem_Old = Relatives.get(Key_i)
-            Val_i = Mem_i[1].split()
+            Val_i = Mem_i[4:].split()
+            print("i:", i, "Mem_Old:", Mem_Old, "Val_i:", Val_i)
             if Mem_Old == None:
                 Sum_i = Val_i
             if Mem_Old != None:
+#                Sum_i = Mem_Old + Val_i
                 Val_i = Val_i + Mem_Old
+#            Sum_i = Mem_Old + Val_i
             Pair_i = {Key_i: Sum_i}
+#            print("in", "Pair_i:", Pair_i)
             Relatives.update(Pair_i)
+#            continue
+#            Val_i.append(Add_i)
         if Key_i not in Keys:
+            Mem_i = Inher[1]
+            Mem_Old = Mem_i.split()
             Val_i = Mem_i
             Pair_i = {Key_i: Val_i}
-        Pars.append(Val_i)
-        Pair_i = {Key_i: Val_i}
-        Relatives.update(Pair_i)
-#print("Keys:", Keys)
-#print("Pars:", Pars)
+            print("not in", "Pair_i:", Pair_i)
+#            continue
+#        Val_i = Mem_i
+#        print("Val_i:", Val_i)
+#        Val_ii = Val_i.split()
+#        Len_ii = len(Val_ii)
+#        Len_ii = len(Val_i)
+#        print("i:", i, "Key_i:", Key_i, "Val_i:", Val_i)
+#        Values.append(Val_i)
+#        Pars.append(Val_ii)
+            Pars.append(Val_i)
+            Pair_i = {Key_i: Val_i}
+            Relatives.update(Pair_i)
+
+print("Keys:", Keys)
+print("Pars:", Pars)
+#print("Values:", Values)
 print("Relatives:")
-print(Relatives)
+#    Pair_k = {Key_k: Val_k}
+#    Relatives.update(Pair_k)
+print("Relatives:", Relatives)
 # Печать словаря Relatives по парам Key:Pars
 N_Keys = len(Keys)
 print("N_Keys:", N_Keys)
 for k in range(N_Keys):
     Key_k = Keys[k]
     Val_k = Relatives.get(Key_k)
+#    Val_k = Pars[k]
     print(Key_k, ":", Val_k)
 # Выполнение запросов
-Req_i = []
+#Req_i = []
 #Again = False
 #while Again == False:
 #    for i in range(Requests):
@@ -87,7 +124,7 @@ Req_i = []
 #        Len_Req = len(Req_i)
 #        if Len_Req == 2:
 #            Father_i = Req_i[0]
-#            Father_i = Req_i[0]
+#            Kid_i = Req_i[1]
 #            Pair_i = {Kid_i: Father_i}
 #        if Len_Req == 1:
 #            Kid_i = Req_i[0]
@@ -110,14 +147,6 @@ Req_i = []
 #            print("Yes")
 #        else:
 #            print("No")
-print("Results:")
-for i in range(Requests):
-    Req_i = Req[i]
-    Father_i = Req_i[0]
-    Kid_i = Req_i[1]
-    Parents_i = Relatives.get(Key_i)
-    print(Req_i, Father_i, Kid_i, Parents_i)
-    if (Father_i in Parents_i) or Father_i == Kid_i:
-        print("Yes")
-    else:
-        print("No")
+
+
+
