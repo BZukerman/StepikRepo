41a2ebe550452c596736de941c590e3bf743fc45
BZukerman <BZukerman@mail.ru>
2020-05-20 11:45:13 +0500

    20/05/2020

--- Python_Programming/Basics and use/ExtendedStack.py
+++ Python_Programming/Basics and use/ExtendedStack.py
@@ -12,27 +12,52 @@
 #
 class ExtendedStack(list):
     def sum(self):      # Операция сложения
+        Length = len(self)
+#        print(Length)
+        if Length == 1:
+            print("Check Length sum")
+            quit()
         First = self.pop()
         Second = self.pop()
         Result = First + Second
         self.append(Result)
     def sub(self):      # Операция вычитания
+        Length = len(self)
+#        print(Length)
+        if Length == 1:
+            print("Check Length sub")
+            quit()
         First = self.pop()
         Second = self.pop()
         Result = First - Second
         self.append(Result)
     def mul(self):      # Операция умножения
+        Length = len(self)
+#        print(Length)
+        if Length == 1:
+            print("Check Length mul")
+            quit()
         First = self.pop()
         Second = self.pop()
         Result = First * Second
         self.append(Result)
     def div(self):      # Операция целочисленного деления
+        Length = len(self)
+#        print(Length)
+        if Length == 1:
+            print("Check Length div")
+            quit()
         First = self.pop()
         Second = self.pop()
+        if Second == 0:
+            print("Zero divide")
+            quit()
         Result = First // Second
         self.append(Result)
 #
+# Array = [1, 2, 3, 4, 5, 6, 7, 0, 9]
 Array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Array = [1, 2, 3, 4]
 print("Array 0:", Array)
 ExtendedStack.div(Array)
 print("Array 1:", Array)
