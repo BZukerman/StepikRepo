4b9e7d4900a0e1471c8fd44f751b28d83905c1ec
BZukerman <BZukerman@mail.ru>
2020-07-01 15:59:17 +0500

    01/07/2020

--- Python_Programming/Basics and use/Test.py
+++ Python_Programming/Basics and use/Test.py
@@ -1,7 +1,9 @@
-Exc = ['Z', 'O', 'A', 'F']
-Par = Exc[1]
-print(Par)
-if Par in Exc:
-    print("Yes")
-else:
-    print("No")
+Extra = ['y', 'm', 'n', 'g']
+Exc_h = 'm'
+Flag = True
+if Flag == True and Exc_h not in Extra:
+  Extra.append(Exc_h)
+print(Extra)
+if Flag == True and Exc_h not in Extra:
+  Extra.append(Exc_h)
+print(Extra)
