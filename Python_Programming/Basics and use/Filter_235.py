2f5a1e4053d3173dbb64c8cedd6bc331e229f310
BZukerman <BZukerman@mail.ru>
2020-08-14 16:15:08 +0500

    14/08/2020

--- Python_Programming/Basics and use/Filter_235.py
+++ Python_Programming/Basics and use/Filter_235.py
@@ -12,15 +12,15 @@
 class multifilter:
     def judge_half(pos, neg):
         # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
-        return pos >= neg
+        return bool(pos >= neg)
 
     def judge_any(pos, neg):
         # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
-        return pos >= 1
+        return bool(pos >= 1)
 
     def judge_all(pos, neg):
         # допускает элемент, если его допускают все функции (neg == 0)
-        return neg == 0
+        return bool(neg == 0)
 
     def __init__(self, iterable, *funcs, judge=judge_any):
         # iterable - исходная последовательность
@@ -32,7 +32,7 @@ class multifilter:
 
     def __iter__(self):
         # возвращает итератор по результирующей последовательности
-        for i in self.iterable:     # Цикл по последовательности
+        for i in self.iterable:     # Цикл по заданной последовательности
             pos = 0                 # Обнуление счетчиков
             neg = 0
             for f in self.funcs:    # Цикл по допускающим функциям
@@ -47,16 +47,16 @@ class multifilter:
                 yield i
 
 def mul2(x):
-    return x % 2 == 0
+    return bool(x % 2 == 0)
 
 def mul3(x):
-    return x % 3 == 0
+    return bool(x % 3 == 0)
 
 def mul5(x):
-    return x % 5 == 0
+    return bool(x % 5 == 0)
 
 a = [i for i in range(31)] # [0, 1, 2, ... , 30]
-# print(a)
+print(a)
 #
 print(list(multifilter(a, mul2, mul3, mul5)))
 #
