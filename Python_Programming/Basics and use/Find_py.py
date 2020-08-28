611c20de5c7304dcc163db7ea514e215536da0a3
BZukerman <BZukerman@mail.ru>
2020-08-28 19:30:47 +0500

    28/08/2020

--- Python_Programming/Basics and use/Find_py.py
+++ Python_Programming/Basics and use/Find_py.py
@@ -2,26 +2,40 @@
 import os
 import os.path
 import shutil
-shutil.unpack_archive('E:\Arc\sample.zip', 'E:\Target', 'zip')
+shutil.unpack_archive('E:\Arc\main.zip', 'E:\Target', 'zip')
 
-Tree = os.walk("E:\Target")
+Tree = os.walk("E:\Target\main")
 # Length = len(Tree)
 # print("Length:", Length)
 Result = []
 Row = []
 for Current_dir, Dir, Files in Tree:
-#    Row.append(i)       #.replace("\\", "/")
-    print(Current_dir, Dir, Files)
-    Row = Files[-3:]
-    print("Row:", Row)
-    for i in Row:
-        print("i:", i)
-        Head_i = i[0]
-        print("Head_i:", Head_i)
-        Tail_i = i[-3:]
-        print("Tail_i:", Tail_i)
-        if Tail_i == ".py":
-            Result.append(i)
-            print("Result:", Result)
+#    print(Current_dir, Dir, Files)
+#    print("Current_dir:", Current_dir)
+    Row = str(Current_dir) + str(Dir) + str(Files)
+#    print("Row:", Row)
+    Head = Current_dir.replace("\\", "/")
+    Row_3 = Files[-3:]
+#    print("Row_3:", Row_3)
+    for i in Row_3:
+#        print("i:", i)
+        Index = i.index(".")
+        Head_i = i[0 : Index]
+        Tail_i = i[Index + 1:]
+#        print(Head_i, Tail_i)
+        if Tail_i == "py":
+#            Result.append(Head_i)
+            Result.append(Head)
+#           print("Result:", Result)
             break
-
+Res_Sort = sorted(Result)
+First = Res_Sort[0]
+Len_F = len(First)
+Pos = First.rindex("/")
+# print("Res_Sort:", Res_Sort)
+f = open("E:\Tsuker\StepikRepo\Text_wr.txt", "w")
+for i in Res_Sort:
+    Slice = str(i[Pos + 1:]) + "\n"
+#    print(Slice)
+    f.write(Slice)
+f.close
