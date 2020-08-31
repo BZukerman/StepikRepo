55ebb5ad2cdcf50db4589be85fe7837c60a3d3b4
BZukerman <BZukerman@mail.ru>
2020-08-31 17:58:07 +0500

    31/08/2020

--- Python_Programming/Basics and use/Find_py.py
+++ Python_Programming/Basics and use/Find_py.py
@@ -1,41 +1,39 @@
 #
-import os
-import os.path
+# import os             # Необязательный шаг
+import os.path          # Импорт модулей
 import shutil
-shutil.unpack_archive('E:\Arc\main.zip', 'E:\Target', 'zip')
-
-Tree = os.walk("E:\Target\main")
-# Length = len(Tree)
-# print("Length:", Length)
-Result = []
+shutil.unpack_archive('E:\Arc\main.zip', 'E:\A', 'zip')     # Разархивация
+#
+Tree = os.walk("E:\A\main")         # Откуда информация для получений дерева папок
+Result = []                         # Вспомогательные списки
 Row = []
-for Current_dir, Dir, Files in Tree:
+for Current_dir, Dir, Files in Tree:    # Цикл по строкам дерева папок
 #    print(Current_dir, Dir, Files)
 #    print("Current_dir:", Current_dir)
-    Row = str(Current_dir) + str(Dir) + str(Files)
+    Row = str(Current_dir) + str(Dir) + str(Files)      # Строка результата
 #    print("Row:", Row)
-    Head = Current_dir.replace("\\", "/")
-    Row_3 = Files[-3:]
+    Head = Current_dir.replace("\\", "/")           # Начало пути с заменой слэшей
+    Row_3 = Files                   # Строка полного пути к файлу
 #    print("Row_3:", Row_3)
-    for i in Row_3:
+    for i in Row_3:                 # Цикл по пути к файлу
 #        print("i:", i)
-        Index = i.index(".")
-        Head_i = i[0 : Index]
-        Tail_i = i[Index + 1:]
+        Index = i.index(".")        # Поиск к разделителю имени и расшиоения
+        Head_i = i[0: Index]        # Полный путь к файлу
+        Tail_i = i[Index + 1:]      # Расширение файла
 #        print(Head_i, Tail_i)
-        if Tail_i == "py":
+        if Tail_i == "py":          # Если это файл исходника Python
 #            Result.append(Head_i)
-            Result.append(Head)
+            Result.append(Head)     # Запись пути к файлу Python
 #           print("Result:", Result)
-            break
-Res_Sort = sorted(Result)
-First = Res_Sort[0]
-Len_F = len(First)
-Pos = First.rindex("/")
+            break                   # Выход из цикла, т.к. найден файл Python
+Res_Sort = sorted(Result)           # Сортировка результата
+First = Res_Sort[0]                 #
+# Len_F = len(First)                  #
+Pos = First.rindex("/")             # Индекс первого справа слеша
 # print("Res_Sort:", Res_Sort)
-f = open("E:\Tsuker\StepikRepo\Text_wr.txt", "w")
-for i in Res_Sort:
-    Slice = str(i[Pos + 1:]) + "\n"
+f = open("E:\Tsuker\StepikRepo\Text_wr_utf.txt", "w", encoding = "utf-8")
+for i in Res_Sort:                  # Цикл по строкам
+    Slice = str(i[Pos + 1:]) + "\n" # Срез строки, начиная после слеша
 #    print(Slice)
-    f.write(Slice)
-f.close
+    f.write(Slice)                  # Запись строки в яайл
+f.close                             # Закрытие файла
---
+++ Python_Programming/Basics and use/Find_py_1.py
@@ -0,0 +1,42 @@
+#
+# import os
+import os.path
+import shutil
+shutil.unpack_archive('E:\Arc\main.zip', 'E:\A', 'zip')
+#
+Tree = os.walk("E:\A\main")
+# Length = len(Tree)
+# print("Length:", Length)
+Result = []
+Row = []
+for Current_dir, Dir, Files in Tree:
+#    print(Current_dir, Dir, Files)
+#    print("Current_dir:", Current_dir)
+    Row = str(Current_dir) + str(Dir) + str(Files)
+#    print("Row:", Row)
+    Head = Current_dir.replace("\\", "/")
+    Row_3 = Files
+#    print("Row_3:", Row_3)
+    for i in Row_3:
+#        print("i:", i)
+        Index = i.index(".")
+        Head_i = i[0: Index]
+        Tail_i = i[Index + 1:]
+#        print(Head_i, Tail_i)
+        if Tail_i == "py":
+#            Result.append(Head_i)
+            Result.append(Head)
+#           print("Result:", Result)
+            break
+# Res_Sort = sorted(Result)       # Сортировка результата
+Res_Sort = Result               # Без сортировки результата
+First = Res_Sort[0]
+Len_F = len(First)
+Pos = First.rindex("/")
+# print("Res_Sort:", Res_Sort)
+f = open("E:\Tsuker\StepikRepo\Text_wr_1.txt", "w")
+for i in Res_Sort:
+    Slice = str(i[Pos + 1:]) + "\n"
+#    print(Slice)
+    f.write(Slice)
+f.close
