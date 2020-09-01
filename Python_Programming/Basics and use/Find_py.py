3350c5ba463b6b891400c5c840a73c7938446864
BZukerman <BZukerman@mail.ru>
2020-09-01 10:44:57 +0500

    01/09/2020

--- Python_Programming/Basics and use/Find_py.py
+++ Python_Programming/Basics and use/Find_py.py
@@ -1,3 +1,8 @@
+# Вам дана в архиве (ссылка) файловая структура, состоящая из директорий и файлов.
+# Вам необходимо распаковать этот архив, и затем найти в данной в файловой структуре
+# все директории, в которых есть хотя бы один файл с расширением ".py".
+# Ответом на данную задачу будет являться файл со списком таких директорий,
+# отсортированных в лексикографическом порядке.
 #
 # import os             # Необязательный шаг
 import os.path          # Импорт модулей
@@ -6,32 +11,30 @@ shutil.unpack_archive('E:\Arc\main.zip', 'E:\A', 'zip')     # Разархива
 #
 Tree = os.walk("E:\A\main")         # Откуда информация для получений дерева папок
 Result = []                         # Вспомогательные списки
-Row = []
+# Row = []
 for Current_dir, Dir, Files in Tree:    # Цикл по строкам дерева папок
 #    print(Current_dir, Dir, Files)
 #    print("Current_dir:", Current_dir)
-    Row = str(Current_dir) + str(Dir) + str(Files)      # Строка результата
+#    Row = str(Current_dir) + str(Dir) + str(Files)      # Строка результата
 #    print("Row:", Row)
-    Head = Current_dir.replace("\\", "/")           # Начало пути с заменой слэшей
-    Row_3 = Files                   # Строка полного пути к файлу
-#    print("Row_3:", Row_3)
-    for i in Row_3:                 # Цикл по пути к файлу
+    Head = Current_dir.replace("\\", "/")       # Начало пути с заменой слэшей
+#                                               Files - cтрока полного пути к файлу
+    for i in Files:                 # Цикл по пути к файлу
 #        print("i:", i)
         Index = i.index(".")        # Поиск к разделителю имени и расшиоения
-        Head_i = i[0: Index]        # Полный путь к файлу
+#        Head_i = i[0: Index]        # Полный путь к файлу
         Tail_i = i[Index + 1:]      # Расширение файла
 #        print(Head_i, Tail_i)
         if Tail_i == "py":          # Если это файл исходника Python
-#            Result.append(Head_i)
             Result.append(Head)     # Запись пути к файлу Python
 #           print("Result:", Result)
             break                   # Выход из цикла, т.к. найден файл Python
 Res_Sort = sorted(Result)           # Сортировка результата
-First = Res_Sort[0]                 #
-# Len_F = len(First)                  #
+First = Res_Sort[0]                 # Первый элемент списка
 Pos = First.rindex("/")             # Индекс первого справа слеша
 # print("Res_Sort:", Res_Sort)
 f = open("E:\Tsuker\StepikRepo\Text_wr_utf.txt", "w", encoding = "utf-8")
+                                    # Последний параметр необязателен
 for i in Res_Sort:                  # Цикл по строкам
     Slice = str(i[Pos + 1:]) + "\n" # Срез строки, начиная после слеша
 #    print(Slice)
