823802e919ea46f85532ba2c6f3df89572cf4416
BZukerman <BZukerman@mail.ru>
2020-09-09 13:14:54 +0500

    09/09/2020

---
+++ Python_Programming/Basics and use/Mod_Checker.py
@@ -0,0 +1,12 @@
+def mod_checker(x, mod = 0):        # Default mod = 0
+#    return lambda y: y % x == mod
+    return lambda y: True if y % x == mod else False    # Более понятно
+
+mod_3 = mod_checker(3)              # f(3, 0)
+print(mod_3(3))     # True          # Остаток от (3 % 3) равен 0
+print(mod_3(2))     # False         # Остаток от (3 % 2) равен 1
+
+mod_3_1 = mod_checker(3, 1)         # f(3, 1)
+print(mod_3_1(4))   # True          # Остаток от (4 % 3) равен 1
+print(mod_3_1(3))   # False         # Остаток от (3 % 3) равен 0
+print(mod_3_1(2))   # False         # Остаток от (2 % 3) равен 2
