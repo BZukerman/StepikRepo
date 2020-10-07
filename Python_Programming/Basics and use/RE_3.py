9b2633a88a18380800be4af5aabcdf1ee4b136f2
BZukerman <BZukerman@mail.ru>
2020-10-07 19:54:23 +0500

    07/10/2020

--- Python_Programming/Basics and use/RE_3.py
+++ Python_Programming/Basics and use/RE_3.py
@@ -1,1 +1,16 @@
 #
+import sys
+import re
+#
+pattern = r".*?z(.{3})z.*"  # Шаблон: слева любые символы и "z"
+#                           # .*? : ? - защита от "жадности" RE
+#                           # Справа "z" и любые символы
+for line in sys.stdin:      # Цикл ввода
+    line = line.rstrip()    # текущая строка
+    if line == "":          # Продолжать при вводе пустой строки
+        continue
+    Check = re.search(pattern, line)    # Проверка строки по шаблону
+    if Check is not None:       # Если подстрока удовлетворяет шаблону
+        print(Check.group())    # Печать подстроки
+    else:                       # Печать при отладке
+        print("None")
