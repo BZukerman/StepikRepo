d5a60f3267d84a2bc736a0b872265125f45ae306
BZukerman <BZukerman@mail.ru>
2020-11-23 16:20:13 +0500

    23/11/2020

--- Python_Programming/Basics and use/RE_8.py
+++ Python_Programming/Basics and use/RE_8.py
@@ -1,13 +1,19 @@
 #
+# Вам дана последовательность строк.
+# В каждой строке поменяйте местами две первых буквы в каждом слове,
+# состоящем хотя бы из двух букв.
+# Буквой считается символ из группы \w.
+#
 import sys
 import re
 #
-pattern = r"\b([aA]+)\b.*?" # Что искать?
-repl = r"argh"              # На что заменить?
+pattern = r"\b(\w)(\w)(\w*)\b"  # Что искать?
+repl = r"\2\1\3"                # На что заменить?
+
 for line in sys.stdin:      # Цикл ввода
     line = line.rstrip()    # Tекущая строка
     if line == "":          # Продолжать при вводе пустой строки
         continue
-    Result = re.sub(pattern, repl, line, count = 1, flags = re.IGNORECASE)
+    Result = re.sub(pattern, repl, line, flags = re.IGNORECASE)
         # Замена первого вхождения pattern на repl в line, регистр - любой
     print(Result)           # Печать строки
