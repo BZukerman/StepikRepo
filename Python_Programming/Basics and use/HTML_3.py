4b6daa4f3c63cf013c86a22024bd58057942c88f
BZukerman <BZukerman@mail.ru>
2020-12-15 21:24:07 +0500

    15/12/2020

--- Python_Programming/Basics and use/HTML_3.py
+++ Python_Programming/Basics and use/HTML_3.py
@@ -7,75 +7,94 @@
 import re
 import requests
 #
-def calc(lin, URL):             # Повторяющиеся операции
+def calc(URL):                  # Повторяющиеся операции
     global Resource, Text       # Глобальные переменые
 #
     Res = requests.get(URL)     # Запрос к URL
-    print("Calc_Res:", Res)
+#    print("Calc_Res:", Res)
     SC = Res.status_code        # Получить статус-код
     if SC == 404 or SC == 500:  # Ресурс не найден или ошибка на сервере
-        print("Calc_Status_Code:", SC)
+#        print("Calc_Status_Code:", SC)
         print("Calc_No")
         quit()
     Text = Res.text             # Выделение текстовой информации
     Resource = re.findall(Pattern1, Text)   # Ресурс запроса
     return
 #
-Pattern1 = r"(<a.*? href=(.*))a>"     # Паттерн для поиска тега - не используется
-Pattern2 = r"sample\d.html"     # Паттерн для поиска ресурса
-Pattern3 = r"https.*.html"      # Паттерн для поиска пути
+Pattern1 = r"(<a.*? href=(.*))a>"       # Паттерн для поиска тега
+# Pattern2 = r"sample\d.html"           # Паттерн для поиска ресурса (d.html)
+Pattern3 = r"https.*.html"              # Паттерн для поиска пути
 #
 WaysA = []
 WaysB = []
 TargetsA = []
+Flag = False
 #
 line = input()                  # Ввод URLA
 URLA = line.rstrip()            # Удаление пробелов в конце строки
 # TailA = re.findall(Pattern2, URLA)  # Выделение ресурса (d.html)
-calc(line, URLA)                # Работа функции
+calc(URLA)                      # Работа функции
 ResourceA = Resource            # Получение глобальной переменной
 TextA = Text                    # Получение глобальной переменной
+#print("TextA:", TextA)
 WayA = re.findall(Pattern3, TextA)  # Полный путь к ресурсу
 print("WayA:", WayA)
-WaysA.extend(WayA)               # Накопление путей в массив
+WaysA.extend(WayA)              # Накопление путей в массив
 print("WaysA:", WaysA)
 #
 line = input()                  # Ввод URLB
 URLB = line.rstrip()            # Удаление пробелов в конце строки
-print("URLB:", URLB)
+#print("URLB:", URLB)
 #TailB = re.findall(Pattern2, URLB)  # Выделение ресурса (d.html)
 #calc(line, URLB)         # Работа функции
 #ResourceB = Resource            # Получение глобальной переменной
-TextB = Text                    # Получение глобальной переменной
-WayB = re.findall(Pattern3, TextB)  # Полный путь к ресурсу
-print("WayB:", WayB)
-#WaysB.extend(WayB)              # Накопление путей в массив
+#TextB = Text                    # Получение глобальной переменной
+#WayB = re.findall(Pattern3, TextB)  # Полный путь к ресурсу
+#print("WayB:", WayB)
+#WaysB.extend(WayB)                # Накопление путей в массив
+WaysB.append(URLB)              # Путь по ссылке В
 #print("WaysB:", WaysB)
 #
 LengthA = len(WaysA)
 # print("LengthA:", LengthA)
 for i in range(LengthA):
     ResAi = requests.get(WaysA[i])
+#    ResAi = WaysA[i]
 #    print("ResAi:", ResAi)
 #    ResAi = Req1i.text
 #    print("Res1i:", Res1i)
     SCi = ResAi.status_code
     TextAi = ResAi.text
-    print("TextAi:", TextAi)
-    print("SCi:", SCi)
-    Resource1i = re.findall(Pattern2, TextAi)
+#    print("TextAi:", TextAi)
+#    print("SCi:", SCi)
+#    Resource1i = re.findall(Pattern2, TextAi)
     linei = re.findall(Pattern3, TextAi)
+#    print("linei:", linei)
     if SCi == 200:
         TargetsA.append(linei)
-print("LengthA:", LengthA)
-print("TargetsA:", TargetsA)
+#    else:
+#        Flag = False
+#        continue
+    if WaysB in TargetsA:
+        Flag = True
+        break
 #
-if URLB == TargetsA:        # ???
-    print("URLB in TargetsA")
+if Flag:
+    print("Yes")
 else:
-    print("URLB is not in TargetsA")
-
+    print("No")
 quit()
+
+#print("LengthA:", LengthA)
+#print("TargetsA:", TargetsA)
+#
+#if WaysB in TargetsA:
+#    print("WaysB is in TargetsA")
+#else:
+#    print("WaysB is not in TargetsA")
+#
+#quit()
+
 
 if Resource1 == Tail2 and Resource2 == Tail2:
     print("Yes")
