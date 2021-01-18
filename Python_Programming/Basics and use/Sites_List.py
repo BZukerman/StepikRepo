4aa86fe668c4906c53a8d2d1ca20f45199dffaf4
BZukerman <BZukerman@mail.ru>
2021-01-18 20:49:12 +0500

    18/01/2021

--- Python_Programming/Basics and use/Sites_List.py
+++ Python_Programming/Basics and use/Sites_List.py
@@ -8,25 +8,34 @@ URL = "https://stepic.org/media/attachments/lesson/24471/01"  # Отладка
 Res_New = []        # Вспомогательные списки
 Mem_i = []
 RNL = []
+# List = []
 Respond = requests.get(URL)     # Запрос по заданному URL
+print(type(Respond))
+print("Respond:", Respond)
 SC = Respond.status_code        # Статус-код
-List = Respond.text             # Список ссылок
-# print("List:")
-# print(List)
+# List = Respond.text.splitlines()             # Список ссылок - как список
+List = Respond.text             # Строка списка ссылок
+print("List:")
+print(List)
+print(type(List))
+l = len(List)
+print(l)
 #
-Pattern = r"((\w{1,}\.){1,}\w{1,})"     # Паттерн поиска
-Result = re.findall(Pattern, List)      # Список результатлв поиска
-# print("Result:")
-# print(Result)
+# Pattern = r"((\w{1,}\.){1,}\w{1,})"     # Паттерн поиска
+# Pattern = r"(\w*?(-){0,}\w*?\.){1,}(\w*?(-){0,}\w*?){0,}"
+Pattern = "(<a href=(\'|\")){1}(\w*?ps{0,}:\/|/){0,}(\w*?(-){0,}\w*?\.){1,}(\w*(-){0,}\w*?){0,}"
+Result = re.findall(Pattern, List)      # Список результатов поиска
+print("Result:")
+print(Result)
 Length = len(Result)            # Длина списка результата поиска
 # print("Length:", Length)
 for i in range(Length):         # Цикл по элементам списка
     Mem_i = Result[i]           # Группы совпадения
-#    print("Mem_i:", Mem_i)
-    Len_i = len(Mem_i)          # Длинс группц совпадения
+    print("Mem_i:", Mem_i)
+    Len_i = len(Mem_i)          # Длина группы совпадения
 #    print("Len_i:", Len_i)
     Res_New = Mem_i[0]          # Выделение Full match совпадения ([0])
-#    print(Res_New)
+    print(Res_New)
     RNL.append(Res_New)         # Заполнение списка Full match совпадениями
 # print("RNL:")
 # print(RNL)
