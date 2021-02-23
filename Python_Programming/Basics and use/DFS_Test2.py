36754db210553ef94676643363cf432e0c8b453a
BZukerman <BZukerman@mail.ru>
2021-02-23 11:04:02 +0500

    23/02/2021

--- Python_Programming/Basics and use/DFS_Test2.py
+++ Python_Programming/Basics and use/DFS_Test2.py
@@ -1,12 +1,14 @@
 # Алшоритм DFS заимствован из источника:
 # https://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
 #
-graph = {'A': set(['B', 'C']),
+graph1 = {'A': set(['B', 'C']),
          'B': set(['A', 'D', 'E']),
          'C': set(['A', 'F']),
          'D': set(['B']),
          'E': set(['B', 'F']),
          'F': set(['C', 'E'])}
+#
+graph2 = {'B': set(['A', 'C']), 'C': set(['A']), 'A': set([]), 'D': set(['C', 'F']), 'E': set(['D']), 'F': set([])}
 #
 def dfs(graph, start, visited=None):
     if visited is None:
@@ -16,6 +18,10 @@ def dfs(graph, start, visited=None):
         dfs(graph, next, visited)
     return visited
 #
-L = dfs(graph, 'A')
+L = dfs(graph1, 'F')
 # print(dfs(graph, 'A'))
+print(L)
+# quit()
+print()
+L = dfs(graph2, 'F')
 print(L)
