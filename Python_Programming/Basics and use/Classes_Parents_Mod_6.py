185672a4a07417858456cc5f0be401e5da597bf4
BZukerman <BZukerman@mail.ru>
2021-03-09 16:32:43 +0500

    09/03/2021

--- Python_Programming/Basics and use/Classes_Parents_Mod_6.py
+++ Python_Programming/Basics and use/Classes_Parents_Mod_6.py
@@ -2,8 +2,9 @@
 # NewDict и Relatives словари {Kid: Parents}
 #
 # J_Data = '[{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]' # 312
-J_Data = '[{"name": "BB", "parents": ["AA", "CC"]}, {"name": "CC", "parents": ["AA"]}, {"name": "AA", "parents": []}, {"name": "DD", "parents":["CC", "FF"]}, {"name": "EE", "parents":["DD"]}, {"name": "FF", "parents":[]}]'   # Анастасия Гончар 514213
-# J_Data = '[{"name": "A", "parents": []}, {"name": "D", "parents": ["A", "B"]}, {"name": "C", "parents": ["E", "D"]}, {"name": "E", "parents": ["A"]}, {"name": "F", "parents": ["C"]}, {"name": "G", "parents": ["D"]}, {"name": "F", "parents": ["G"]}, {"name": "K", "parents": ["E", "B"]}]'     #772441221
+# J_Data = '[{"name": "BB", "parents": ["AA", "CC"]}, {"name": "CC", "parents": ["AA"]}, {"name": "AA", "parents": []}, {"name": "DD", "parents":["CC", "FF"]}, {"name": "EE", "parents":["DD"]}, {"name": "FF", "parents":[]}]'   # Анастасия Гончар 514213
+# J_Data = '[{"name": "A", "parents": []}, {"name": "D", "parents": ["A", "B"]}, {"name": "C", "parents": ["E", "D"]}, {"name": "E", "parents": ["A"]}, {"name": "F", "parents": ["C"]}, {"name": "G", "parents": ["D"]}, {"name": "F", "parents": ["G"]}, {"name": "K", "parents": ["E", "B"]}]'     #77244121
+J_Data = '[{"name": "A", "parents": []}, {"name": "D", "parents": ["A", "B"]}, {"name": "C", "parents": ["E", "D"]}, {"name": "E", "parents": ["A"]}, {"name": "F", "parents": ["C"]}, {"name": "G", "parents": ["D"]}, {"name": "F", "parents": ["G", "XX"]}, {"name": "K", "parents": ["E", "B", "YYY"]}]'  # Мой вариант F: C. G, X ! 7724412133
 # J_Data = '[{"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}, {"name": "A", "parents": []}, {"name": "D", "parents":["C", "F"]}, {"name": "E", "parents":["D"]}, {"name": "F", "parents":[]}]'     # 514213
 # J_Data = '[{"name": "A", "parents": []}, {"name": "B", "parents": ["A"]}, {"name": "C", "parents": ["A"]}, {"name": "D", "parents": ["B", "C"]}, {"name": "V", "parents": ["D"]}]'  # 53321
 # J_Data = '[{"name": "dfgre", "parents": ["gsdfgre"]}, {"name": "hsdgreg", "parents": ["dfgre", "gsd"]}, {"name": "gsd", "parents": ["dfgre"]}, {"name": "gsdfgre", "parents": []}]'     # 3241
@@ -57,7 +58,7 @@ for j in range(Len_D):
 #     print("NewDictj:", NewDictj)
     NewDict.append(NewDictj)
 NewDict = NewDict[1:]                       # Срез словаря (убрать " ")
-# print("NewDict:", NewDict)
+print("NewDict:", NewDict)
 # print("Keys:", Keys)
 Len_K = len(Keys)
 # print("Len_K:", Len_K)
@@ -76,10 +77,21 @@ N_Keys = len(Keys_Short)
 S_D = {}
 super_dict = {}     # Алгоритм объединения нескольких словарей заимствован:
 # https://coderoad.ru/9415785/%D0%BE%D0%B1%D1%8A%D0%B5%D0%B4%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B5%D1%81%D0%BA%D0%BE%D0%BB%D1%8C%D0%BA%D0%B8%D1%85-%D1%81%D0%BB%D0%BE%D0%B2%D0%B0%D1%80%D0%B5%D0%B9-python
+Val_New = []
 for k in set(k for d in NewDict for k in d):
     super_dict[k] = [d[k] for d in NewDict if k in d]
-#
-# print("super_dict:", super_dict)
+    # Проверка числа списков в данных и распаковка в один список
+    Val = super_dict.get(k)
+#    print("Val:", Val)
+    Len_V = len(Val)
+#    print("Len_V:", Len_V)
+    if Len_V > 1:
+        for i in range(Len_V):
+            Val_New.extend(Val[i])
+#            print(Val_New)
+        Pair = {k: [Val_New]}
+        super_dict.update(Pair)
+print("super_dict:", super_dict)
 Len_SD = len(super_dict)
 # print("Len_SD:", Len_SD)
 # quit()
@@ -110,7 +122,7 @@ for i in range(Len_SD):
         Val = L
     Pair = {Key_i: Val}
     S_D.update(Pair)
-# print("S_D:", S_D)
+print("S_D:", S_D)
 Len_SD = len(S_D)
 # print("Len_SD:", Len_SD)
 # quit()
@@ -137,11 +149,13 @@ for i in range(Len_Add):
   Val_i = [Key_i]
   Pair_i = {Key_i: Val_i}
   S_D.update(Pair_i)
-# print("S_D:", S_D)
+print("Extended S_D:", S_D)
+Len_SD = len(S_D)
+# print("Len_SD:", Len_SD)
 # quit()
 #
 Relatives = S_D                             # Relatives - синоним для S_D
-# print("Relatives:", Relatives)
+print("Relatives:", Relatives)
 Length = len(Relatives)
 # print("Length:", Length)
 # quit()
@@ -161,7 +175,7 @@ for k, v in Relatives.items():          # for k, v in Relatives.items():
         New_Relatives[x] = New_Relatives[x] + [k]    # Было: New_Relatives[x] += [k]
     else:
         New_Relatives[x] = [k]
-# print("New_Relatives:", New_Relatives)
+print("New_Relatives:", New_Relatives)
 # quit()
 #
 Other_Keys = []                         # Ключи (кроме текущего)
@@ -178,8 +192,8 @@ for i in range(Length):                 # Цикл по ключам
 #        print("All_Kids_i:", All_Kids_i)
         print(Key_i, ":", 1)
         continue
-    Other_Keys = Keys_Short.copy()            # Соэдание дубля списка ключей (Keys.copy())
-    Other_Keys.remove(Key_i)            # Удаление текущего ключа в дубле
+    Other_Keys = Keys_Short.copy()          # Соэдание дубля списка ключей (Keys.copy())
+    Other_Keys.remove(Key_i)                # Удаление текущего ключа в дубле
 #    print("Other_Keys:", Other_Keys)
     for j in Other_Keys:
         if j in All_Kids_i:
