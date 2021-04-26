958b838d9b999958b1c697d7c448fd6a21eb01d9
BZukerman <BZukerman@mail.ru>
2021-04-26 13:15:29 +0500

    26/04/2021

--- Python_Programming/Basics and use/Classes_Parents_MxMaslin.py
+++ Python_Programming/Basics and use/Classes_Parents_MxMaslin.py
@@ -1,4 +1,4 @@
-#  Решение заимствовано у МxМaslin:
+#  Решение вместо Classes_Parents_Mod_7.py заимствовано у МxМaslin:
 #  https://github.com/mxmaslin/stepik/blob/master/Python%20-%20%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D1%8B%20%D0%B8%20%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5/3.5%20%D0%A0%D0%B0%D1%81%D0%BF%D1%80%D0%BE%D1%81%D1%82%D1%80%D0%B0%D0%BD%D1%91%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D1%8B%20%D1%82%D0%B5%D0%BA%D1%81%D1%82%D0%BE%D0%B2%D1%8B%D1%85%20%D1%84%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2%20CSV%2C%20JSON%204.md
 #
 #  Наборы исходных данных:
